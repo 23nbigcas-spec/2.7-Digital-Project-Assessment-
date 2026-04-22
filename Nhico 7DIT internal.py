32fc75d0a3ad33a2c8fc5851917131145a0aff38
Nhico Bigcas <23nbigcas@wakatipu.school.nz>
2026-04-22 13:49:19 +1200

    started code yessir

--- Nhico 7DIT internal.py
+++ Nhico 7DIT internal.py
@@ -1,1 +1,49 @@
-print("hello")
+orders = [{},{}]
+
+breads = []
+
+proteins = []
+
+vegetables = []
+
+sauces = []
+
+
+def business_menu():
+    print("---- BUSINESS MENU ----")
+    print("1. View Orders")
+    print("2. View Ingredients")
+    print("3. Exit")
+
+
+def ingredients_menu(): #when viewing each category 
+    print("1. View Breads")
+    print("2. View Proteins")
+    print("3. View Greens")
+    print("4. View Sauces")
+
+
+def view_category(category):
+    print(category)
+    print("1. Add")
+    print("2. Remove")
+    print("3. Exit")
+
+
+def add_ingredient(category):
+    print(category)
+    addition = input("What would you like to add?: ")
+    category.append(addition)
+
+def remove_ingredient(category):
+    print(category)
+    remove = input("What would you like to remove?: ")
+    
+    try:
+        category.remove(remove)
+
+    
+    except ValueError:
+        print("Invalid removal, Try again")
+
+
